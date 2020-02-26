04c902292ee6c8e9759e9b7e1da4ea46b6916c58
Jaime Segura <jseguraweb@gmail.com>
2020-02-26 18:35:01 +0100

    Updates

---
+++ precache-manifest.e6c6831e45b7c3d05dd22bdf953b7abc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db5b486aedc511f3416042e16100410",
+    "url": "/fruit-cards-app/index.html"
+  },
+  {
+    "revision": "ba43cb0467aa5064b0e3",
+    "url": "/fruit-cards-app/static/css/main.f159a7f6.chunk.css"
+  },
+  {
+    "revision": "5d10fdefc6562f7f88c3",
+    "url": "/fruit-cards-app/static/js/2.8abefd7d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/fruit-cards-app/static/js/2.8abefd7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba43cb0467aa5064b0e3",
+    "url": "/fruit-cards-app/static/js/main.5e19184a.chunk.js"
+  },
+  {
+    "revision": "4d849fb2ded536812d89",
+    "url": "/fruit-cards-app/static/js/runtime-main.a0e3568c.js"
+  },
+  {
+    "revision": "b2b9f0e82f1bb5d305f7d929e559ac4c",
+    "url": "/fruit-cards-app/static/media/apple.b2b9f0e8.png"
+  },
+  {
+    "revision": "b68ce613e38948927ff9f3fadba634b7",
+    "url": "/fruit-cards-app/static/media/banana.b68ce613.png"
+  },
+  {
+    "revision": "2c13b05b19e4a3de3c9d84fe7d62a9cf",
+    "url": "/fruit-cards-app/static/media/pineapple.2c13b05b.png"
+  }
+]);
